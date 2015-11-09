03a5a26d38b9a87eae825030abe472e3117a1acd
Chai Wei Jun <chaiweijun@Chais-Mac.local>
2015-11-09 17:16:13 +0800

    implemented some front end changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,12 +18,14 @@ class SessionsController < ApplicationController
   def fb_create
   	auth = request.env["omniauth.auth"]
     user = User.from_omniauth(auth)
+    flash[:message] = "Thank you for logging in #{user.full_name}!"
     session[:user_id] = user.id
     redirect_to root_url
   end
 
   def destroy
   	session[:user_id] = nil
+  	flash[:message] = "Logged out!"
   	redirect_to root_path
   end
 end
